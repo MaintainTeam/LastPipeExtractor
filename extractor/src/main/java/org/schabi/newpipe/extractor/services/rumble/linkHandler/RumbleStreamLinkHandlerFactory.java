8e7ea3df42af83861a285f8bd2129c5a56508857
evermind <evermind@tuxfamily.org>
2021-04-26 11:54:59 +0200

    [Rumble] RumbleStreamLinkHandlerFactory

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/linkHandler/RumbleStreamLinkHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/linkHandler/RumbleStreamLinkHandlerFactory.java
@@ -2,10 +2,16 @@ package org.schabi.newpipe.extractor.services.rumble.linkHandler;
 
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.LinkHandlerFactory;
+import org.schabi.newpipe.extractor.utils.Utils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class RumbleStreamLinkHandlerFactory extends LinkHandlerFactory {
 
     private static final RumbleStreamLinkHandlerFactory instance = new RumbleStreamLinkHandlerFactory();
+
+    String BASE_URL = "https://rumble.com";
 
     private RumbleStreamLinkHandlerFactory() {
     }
@@ -15,20 +21,48 @@ public class RumbleStreamLinkHandlerFactory extends LinkHandlerFactory {
     }
 
     private String assertsID(String id) throws ParsingException {
-        return ""; // TODO
+        if (id == null || !id.matches("v[a-zA-Z0-9_-]{5}")) {
+            throw new ParsingException("Given string is not a Rumble Video ID");
+        }
+        return id;
     }
 
     @Override
     public String getUrl(String id) {
-        return ""; // TODO
+        return BASE_URL + "/" + id;
     }
 
     public String getId(String urlString) throws ParsingException, IllegalArgumentException {
-        return ""; // TODO
+        URL url;
+        try {
+            url = Utils.stringToURL(urlString);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("The given URL is not valid");
+        }
+
+        String path = url.getPath();
+
+        if (!path.isEmpty()) {
+            //remove leading "/"
+            path = path.substring(1);
+        }
+
+        try {
+            String[] splitPath = path.split("-", 0);
+            path = splitPath[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ParsingException("Error getting ID");
+        }
+        return assertsID(path);
     }
 
     @Override
     public boolean onAcceptUrl(final String url) throws ParsingException {
-        return false; // TODO
+        try {
+            getId(url);
+            return true;
+        } catch (ParsingException e) {
+            return false;
+        }
     }
 }
