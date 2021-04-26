0f855baa2ad7b570ca02ecda35daa2262045bc7c
evermind <evermind@tuxfamily.org>
2021-04-26 11:20:23 +0200

    [Rumble] unit test for RumbleSearchQueryHandlerFactory

---
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/rumble/linkHandler/RumbleSearchQueryHandlerFactoryTest.java
@@ -0,0 +1,52 @@
+package org.schabi.newpipe.extractor.services.rumble.linkHandler;
+
+
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.schabi.newpipe.extractor.ServiceList.Rumble;
+import static org.schabi.newpipe.extractor.services.rumble.linkHandler.RumbleSearchQueryHandlerFactory.CHANNELS;
+import static org.schabi.newpipe.extractor.services.rumble.linkHandler.RumbleSearchQueryHandlerFactory.VIDEOS;
+import static org.schabi.newpipe.extractor.utils.Utils.EMPTY_STRING;
+
+public class RumbleSearchQueryHandlerFactoryTest {
+
+    @Test
+    public void testRegularValues() throws Exception {
+
+        assertEquals("https://rumble.com/search/video?q=Mark+Dice", Rumble.getSearchQHFactory().fromQuery("Mark Dice").getUrl());
+        assertEquals("https://rumble.com/search/video?q=paul", Rumble.getSearchQHFactory().fromQuery("paul").getUrl());
+    }
+
+    @Test
+    public void testGetContentFilter() throws Exception {
+        assertEquals(VIDEOS, Rumble.getSearchQHFactory()
+                .fromQuery(EMPTY_STRING, asList(new String[]{VIDEOS}), EMPTY_STRING).getContentFilters().get(0));
+        assertEquals(CHANNELS, Rumble.getSearchQHFactory()
+                .fromQuery("asdf", asList(new String[]{CHANNELS}), EMPTY_STRING).getContentFilters().get(0));
+    }
+
+    @Test
+    public void testWithContentfilter() throws Exception {
+        assertEquals("https://rumble.com/search/channel?q=%C3%B6%C3%A4%C3%BC", Rumble.getSearchQHFactory()
+                .fromQuery("öäü", asList(new String[]{CHANNELS}), EMPTY_STRING).getUrl());
+        assertEquals("https://rumble.com/search/video?q=%C3%B6%C3%A4%C3%BC", Rumble.getSearchQHFactory()
+                .fromQuery("öäü", asList(new String[]{VIDEOS}), EMPTY_STRING).getUrl());
+    }
+
+    @Test
+    public void testGetAvailableContentFilter() {
+        final String[] contentFilter = Rumble.getSearchQHFactory().getAvailableContentFilter();
+        assertEquals(3, contentFilter.length);
+        assertEquals("all", contentFilter[0]);
+        assertEquals("videos", contentFilter[1]);
+        assertEquals("channels", contentFilter[2]);
+    }
+
+    @Test
+    public void testGetAvailableSortFilter() {
+        final String[] contentFilter = Rumble.getSearchQHFactory().getAvailableSortFilter();
+        assertEquals(0, contentFilter.length);
+    }
+}
