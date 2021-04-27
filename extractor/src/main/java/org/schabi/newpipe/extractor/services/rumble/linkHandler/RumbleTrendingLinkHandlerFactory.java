089194a4fda5ed75a7540ff61f9446c94e12893d
evermind <evermind@tuxfamily.org>
2021-04-27 14:50:46 +0200

    [Rumble] implementation for RumbleTrendingLinkHandlerFactory

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/linkHandler/RumbleTrendingLinkHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/linkHandler/RumbleTrendingLinkHandlerFactory.java
@@ -2,12 +2,38 @@ package org.schabi.newpipe.extractor.services.rumble.linkHandler;
 
 import org.schabi.newpipe.extractor.linkhandler.ListLinkHandlerFactory;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class RumbleTrendingLinkHandlerFactory extends ListLinkHandlerFactory {
 
+    public static final String LIVE = "Live";
+    public static final String EDITOR_PICKS = "Editor Picks";
+    public static final String NEWS = "News";
+    public static final String VIRAL = "Viral";
+    public static final String PODCASTS = "Podcasts";
+    public static final String TODAYS_BATTLE_LEADERBOARD_TOP_50 = "Today's Battle Leaderboard Top 50";
+    public static final String SPORTS = "Sports";
+
+    final private List<String> trendingIdList = new LinkedList<>();
+    final private Map<String, String> trendingId2UrlMap = new HashMap();
+    final private Map<String,String> trendingUrl2IdMap = new HashMap();
+
     private RumbleTrendingLinkHandlerFactory() {
-        // TODO
+        trendingUrl2IdMap.put("https://rumble.com/live-videos", LIVE);
+        trendingUrl2IdMap.put("https://rumble.com/editor-picks", EDITOR_PICKS);
+        trendingUrl2IdMap.put("https://rumble.com/category/news", NEWS);
+        trendingUrl2IdMap.put("https://rumble.com/category/viral", VIRAL);
+        trendingUrl2IdMap.put("https://rumble.com/category/podcasts", PODCASTS);
+        trendingUrl2IdMap.put("https://rumble.com/battle-leaderboard", TODAYS_BATTLE_LEADERBOARD_TOP_50);
+        trendingUrl2IdMap.put("https://rumble.com/category/sports", SPORTS);
+
+        for(Map.Entry<String, String> entry : trendingUrl2IdMap.entrySet()){
+            trendingId2UrlMap.put(entry.getValue(), entry.getKey());
+            trendingIdList.add(entry.getValue());
+        }
     }
 
     public static RumbleTrendingLinkHandlerFactory instance = new RumbleTrendingLinkHandlerFactory();
@@ -17,20 +43,22 @@ public class RumbleTrendingLinkHandlerFactory extends ListLinkHandlerFactory {
     }
 
     public List<String> getTrendingKioskIdsList() {
-        return null; // TODO
+        return trendingIdList;
     }
 
     public String getUrl(String id, List<String> contentFilters, String sortFilter) {
-        return ""; // TODO
+        return trendingId2UrlMap.get(id);
     }
 
     @Override
     public String getId(String url) {
-        return ""; // TODO
+        return trendingUrl2IdMap.get(url);
     }
 
     @Override
     public boolean onAcceptUrl(final String url) {
-        return false; // TODO
+        if (trendingUrl2IdMap.containsKey(url))
+            return true;
+        return false;
     }
 }
