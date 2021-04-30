50d2e7b75cce507c3e152b5933246dd6248cbdad
evermind <evermind@tuxfamily.org>
2021-04-30 11:17:52 +0200

    [Rumble] extract code from RumbleTrendingExtractor that is usable with RumbleChannelExtractor

---
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/extractors/RumbleCommonCodeTrendingAndChannel.java
@@ -0,0 +1,45 @@
+package org.schabi.newpipe.extractor.services.rumble.extractors;
+
+import org.jsoup.nodes.Document;
+import org.schabi.newpipe.extractor.ListExtractor;
+import org.schabi.newpipe.extractor.Page;
+import org.schabi.newpipe.extractor.exceptions.ExtractionException;
+import org.schabi.newpipe.extractor.stream.StreamInfoItem;
+import org.schabi.newpipe.extractor.stream.StreamInfoItemExtractor;
+import org.schabi.newpipe.extractor.stream.StreamInfoItemsCollector;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * shared code for {@link RumbleTrendingExtractor} and {@link RumbleChannelExtractor}
+ */
+public class RumbleCommonCodeTrendingAndChannel {
+    private final int serviceId;
+    private final String baseUrl;
+    private final RumbleCommonCodeTrendingAndSearching rumbleCommonCodeTrendingAndSearching;
+
+    RumbleCommonCodeTrendingAndChannel(int serviceId,
+                                       String baseUrl) {
+        this.serviceId = serviceId;
+        this.rumbleCommonCodeTrendingAndSearching = new RumbleCommonCodeTrendingAndSearching();
+        this.baseUrl = baseUrl;
+    }
+
+    public ListExtractor.InfoItemsPage<StreamInfoItem> extractAndGetInfoItemsFromPage(Document doc) throws IOException, ExtractionException {
+        final StreamInfoItemsCollector collector = new StreamInfoItemsCollector(serviceId);
+
+        List<StreamInfoItemExtractor> infoItemsList =
+                rumbleCommonCodeTrendingAndSearching.getSearchOrTrendingResultsItemList(doc);
+
+        for (StreamInfoItemExtractor infoItemExtractor : infoItemsList) {
+            collector.commit(infoItemExtractor);
+        }
+
+        Page nextPage = rumbleCommonCodeTrendingAndSearching
+                .getNewPageIfThereAreMoreThanOnePageResults(
+                        infoItemsList.size(), doc, baseUrl + "?page=" );
+
+        return new ListExtractor.InfoItemsPage<>(collector, nextPage);
+    }
+}
--- extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/extractors/RumbleTrendingExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/extractors/RumbleTrendingExtractor.java
@@ -10,55 +10,31 @@ import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.kiosk.KioskExtractor;
 import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
 import org.schabi.newpipe.extractor.stream.StreamInfoItem;
-import org.schabi.newpipe.extractor.stream.StreamInfoItemExtractor;
-import org.schabi.newpipe.extractor.stream.StreamInfoItemsCollector;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.annotation.Nonnull;
 
 public class RumbleTrendingExtractor extends KioskExtractor<StreamInfoItem> {
 
+    private RumbleCommonCodeTrendingAndChannel sharedTrendingAndChannelCode;
     private Document doc;
-    private RumbleCommonCodeTrendingAndSearching rumbleCommonCodeTrendingAndSearching;
 
     public RumbleTrendingExtractor(StreamingService service,
                                     ListLinkHandler linkHandler,
                                     String kioskId) {
         super(service, linkHandler, kioskId);
-        rumbleCommonCodeTrendingAndSearching = new RumbleCommonCodeTrendingAndSearching();
+
+        try {
+            sharedTrendingAndChannelCode = new RumbleCommonCodeTrendingAndChannel(getServiceId(), getUrl());
+        } catch (ParsingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onFetchPage(@Nonnull Downloader downloader) throws IOException, ExtractionException {
         doc = Jsoup.parse(getDownloader().get(getUrl()).responseBody());
-    }
-
-    @Override
-    public InfoItemsPage<StreamInfoItem> getPage(final Page page) throws IOException, ExtractionException {
-        if (null == page)
-            return null;
-
-        doc = Jsoup.parse(getDownloader().get(page.getUrl()).responseBody());
-        return extractAndGetInfoItemsFromPage();
-    }
-
-    private InfoItemsPage<StreamInfoItem> extractAndGetInfoItemsFromPage() throws IOException, ExtractionException {
-        final StreamInfoItemsCollector collector = new StreamInfoItemsCollector(getServiceId());
-
-        List<StreamInfoItemExtractor> infoItemsList =
-                rumbleCommonCodeTrendingAndSearching.getSearchOrTrendingResultsItemList(doc);
-
-        for (StreamInfoItemExtractor infoItemExtractor : infoItemsList) {
-            collector.commit(infoItemExtractor);
-        }
-
-        Page nextPage = rumbleCommonCodeTrendingAndSearching
-                .getNewPageIfThereAreMoreThanOnePageResults(
-                infoItemsList.size(), doc, getUrl()+ "?page=" );
-
-        return new InfoItemsPage<>(collector, nextPage);
     }
 
     @Nonnull
@@ -67,9 +43,18 @@ public class RumbleTrendingExtractor extends KioskExtractor<StreamInfoItem> {
         return getId();
     }
 
+    @Override
+    public InfoItemsPage<StreamInfoItem> getPage(final Page page) throws IOException, ExtractionException {
+        if (null == page)
+            return null;
+
+        doc = Jsoup.parse(getDownloader().get(page.getUrl()).responseBody());
+        return sharedTrendingAndChannelCode.extractAndGetInfoItemsFromPage(doc);
+    }
+
     @Nonnull
     @Override
     public InfoItemsPage<StreamInfoItem> getInitialPage() throws IOException, ExtractionException {
-        return extractAndGetInfoItemsFromPage();
+        return sharedTrendingAndChannelCode.extractAndGetInfoItemsFromPage(doc);
     }
 }
