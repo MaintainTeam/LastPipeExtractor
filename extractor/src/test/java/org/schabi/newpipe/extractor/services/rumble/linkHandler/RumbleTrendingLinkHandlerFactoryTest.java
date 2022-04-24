391fb0a44da8f4cba9eaa47d4d15f2b9abc978ac
evermind <evermind@tuxfamily.org>
2022-04-24 08:14:07 +0200

    [Rumble] RumbleTrendingLinkHandlerFactoryTest: update default kiosk

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/rumble/linkHandler/RumbleTrendingLinkHandlerFactoryTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/rumble/linkHandler/RumbleTrendingLinkHandlerFactoryTest.java
@@ -23,47 +23,49 @@ import static org.schabi.newpipe.extractor.ServiceList.Rumble;
 public class RumbleTrendingLinkHandlerFactoryTest {
     private static LinkHandlerFactory linkHandlerFactory;
 
-    private final static String LIVE_KEY = "https://rumble.com/live-videos";
-    private final static String EDITOR_PICKS_KEY = "https://rumble.com/editor-picks";
+    private static final String LIVE_KEY = "https://rumble.com/live-videos";
+    private static final String BATTLE_LEADERBOARD_KEY = "https://rumble.com/battle-leaderboard";
 
 
-    private static Map<String,String> expectedTrendingUrl2IdMap = new HashMap()
-    {{
+    private static Map<String, String> expectedTrendingUrl2IdMap = new HashMap() {{
         put(LIVE_KEY,                                 "Live");
-        put(EDITOR_PICKS_KEY,                         "Editor Picks");
+        put("https://rumble.com/editor-picks",      "Editor Picks");
         put("https://rumble.com/category/news",     "News");
         put("https://rumble.com/category/viral",    "Viral");
         put("https://rumble.com/category/podcasts", "Podcasts");
-        put("https://rumble.com/battle-leaderboard","Today's Battle Leaderboard Top 50");
+        put(BATTLE_LEADERBOARD_KEY, "Today's Battle Leaderboard Top 50");
         put("https://rumble.com/category/sports",   "Sports");
     }};
 
     @BeforeAll
     public static void setUp() throws Exception {
         // get the one and only instance. All 'trending' thingies use the same.
-        linkHandlerFactory = Rumble.getKioskList().getListLinkHandlerFactoryByType(expectedTrendingUrl2IdMap.get(LIVE_KEY));
+        linkHandlerFactory = Rumble.getKioskList().getListLinkHandlerFactoryByType(
+                expectedTrendingUrl2IdMap.get(LIVE_KEY));
         NewPipe.init(DownloaderTestImpl.getInstance());
     }
 
     @Test
     public void getUrl() throws Exception {
-        // assume EDITOR_PICKS_KEY is set as DEFAULT_TRENDING in RumbleTrendingLinkHandlerFactory
-        assertEquals(EDITOR_PICKS_KEY, linkHandlerFactory.fromId("").getUrl());
+        // assume TODAYS_BATTLE_LEADERBOARD_TOP_50 is set as DEFAULT_TRENDING
+        // in RumbleTrendingLinkHandlerFactory
+        assertEquals(BATTLE_LEADERBOARD_KEY, linkHandlerFactory.fromId("").getUrl());
     }
 
     @Test
     public void getId() throws Exception {
 
-        for (Map.Entry<String, String> entry : expectedTrendingUrl2IdMap.entrySet()) {
+        for (final Map.Entry<String, String> entry : expectedTrendingUrl2IdMap.entrySet()) {
             assertEquals(linkHandlerFactory.fromUrl(entry.getKey()).getId(), entry.getValue());
-            assertThrows(ParsingException.class, () -> linkHandlerFactory.fromUrl(entry.getValue()).getId());
+            assertThrows(ParsingException.class,
+                    () -> linkHandlerFactory.fromUrl(entry.getValue()).getId());
         }
     }
 
     @Test
     public void acceptUrl() throws ParsingException {
 
-        for (Map.Entry<String, String> entry : expectedTrendingUrl2IdMap.entrySet()) {
+        for (final Map.Entry<String, String> entry : expectedTrendingUrl2IdMap.entrySet()) {
             assertTrue(linkHandlerFactory.acceptUrl(entry.getKey()));
             assertFalse(linkHandlerFactory.acceptUrl(entry.getValue()));
         }
