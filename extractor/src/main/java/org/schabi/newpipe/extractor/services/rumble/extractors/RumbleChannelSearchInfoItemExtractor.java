9bc1b7ed27cf8824ce06d5e6120e0634a185be3f
evermind <evermind@tuxfamily.org>
2022-05-07 22:30:08 +0200

    [Rumble] (1/2) implement Channel search extraction functionality

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/extractors/RumbleChannelSearchInfoItemExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/rumble/extractors/RumbleChannelSearchInfoItemExtractor.java
@@ -1,21 +1,76 @@
 package org.schabi.newpipe.extractor.services.rumble.extractors;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.schabi.newpipe.extractor.channel.ChannelInfoItemExtractor;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
+import org.schabi.newpipe.extractor.services.rumble.RumbleParsingHelper;
+import org.schabi.newpipe.extractor.utils.Utils;
+
+import static org.schabi.newpipe.extractor.ListExtractor.ITEM_COUNT_UNKNOWN;
+import static org.schabi.newpipe.extractor.ServiceList.Rumble;
 
 class RumbleChannelSearchInfoItemExtractor implements ChannelInfoItemExtractor {
 
-    String description;
-    String name;
-    String url;
-    String thumbUrl;
+    private long subscriberCount;
+    private String description = "";
+    private String name;
+    private String url;
+    private String thumbUrl;
+    private boolean verified;
 
-    RumbleChannelSearchInfoItemExtractor(final String name, final String url,
-                                         final String thumbUrl, final String description) {
-        this.description = description;
-        this.name = name;
-        this.url = url;
-        this.thumbUrl = thumbUrl;
+    RumbleChannelSearchInfoItemExtractor(final Element element, final Document doc)
+            throws ParsingException {
+        extractData(element, doc);
+    }
+
+    private void extractData(final Element element, final Document doc) throws ParsingException {
+        this.name = RumbleParsingHelper.extractSafely(true,
+                "Could not extract the channel name",
+                () -> element.select("h3.channel-item--title").text());
+
+        this.subscriberCount = extractSubscriberCount(element, doc);
+
+        this.url = RumbleParsingHelper.extractSafely(true,
+                "Could not extract the stream url",
+                () -> Rumble.getBaseUrl() + element
+                        .select("a.channel-item--a").first().attr("href"));
+
+        this.thumbUrl = extractTheThumbnailOfAChannelInASearchForChannels(element, doc);
+
+        this.verified = !element.select("svg.channel-item--by-verified").isEmpty();
+    }
+
+    private long extractSubscriberCount(final Element element, final Document document)
+            throws ParsingException {
+
+        final String errorMsg = "Could not get subscriber count";
+        final String amountOfSubscribers = RumbleParsingHelper.extractSafely(true,
+                errorMsg,
+                () -> element.select("span.channel-item--subscribers").first().text());
+
+        if (null != amountOfSubscribers) {
+            try {
+                return Utils.mixedNumberWordToLong(amountOfSubscribers.replace(",", ""));
+            } catch (final NumberFormatException e) {
+                throw new ParsingException(errorMsg, e);
+            }
+        } else {
+            return ITEM_COUNT_UNKNOWN;
+        }
+    }
+
+    // extract the thumbnail of a channel in a channel search
+    private String extractTheThumbnailOfAChannelInASearchForChannels(final Element element,
+                                                                     final Document document)
+            throws ParsingException {
+        return RumbleParsingHelper.extractThumbnail(document, element.toString(),
+                () -> {
+                    final String thumbUrlIdentifier = "i." + element
+                            .select("i.user-image").attr("class")
+                            .split("user-image user-image--img ")[1];
+                    return thumbUrlIdentifier;
+                });
     }
 
     @Override
@@ -25,7 +80,7 @@ class RumbleChannelSearchInfoItemExtractor implements ChannelInfoItemExtractor {
 
     @Override
     public long getSubscriberCount() throws ParsingException {
-        return -1;
+        return subscriberCount;
     }
 
     @Override
@@ -35,7 +90,7 @@ class RumbleChannelSearchInfoItemExtractor implements ChannelInfoItemExtractor {
 
     @Override
     public boolean isVerified() throws ParsingException {
-        return false;
+        return verified;
     }
 
     @Override
