1c9c1aac301f71a1cb09f039879e2b1cc05778e5
evermind <evermind@tuxfamily.org>
2022-08-19 00:45:53 +0200

    searchfilters: convert Peertube to new framework
    Available content filters:
    - All
    - Videos
    - Channels
    - Playlists
    Sepia
    - SepiaSearch
    
    Available sort filters:
    - 'Sort by'
    - stream kind
    - sensitive
    - 'Date'
    - 'Duration'

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/peertube/linkHandler/PeertubeSearchQueryHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/peertube/linkHandler/PeertubeSearchQueryHandlerFactory.java
@@ -4,18 +4,23 @@ import org.schabi.newpipe.extractor.ServiceList;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
 import org.schabi.newpipe.extractor.utils.Utils;
+import org.schabi.newpipe.extractor.search.filter.FilterItem;
+import org.schabi.newpipe.extractor.services.peertube.PeertubeHelpers;
+import org.schabi.newpipe.extractor.services.peertube.search.filter.PeertubeFilters;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Optional;
 
 public final class PeertubeSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
 
     public static final String VIDEOS = "videos";
-    public static final String SEPIA_VIDEOS = "sepia_videos"; // sepia is the global index
+    // sepia is the global index
     public static final String SEPIA_BASE_URL = "https://sepiasearch.org";
     public static final String SEARCH_ENDPOINT = "/api/v1/search/videos";
 
     private PeertubeSearchQueryHandlerFactory() {
+        super(new PeertubeFilters());
     }
 
     public static PeertubeSearchQueryHandlerFactory getInstance() {
@@ -24,34 +29,36 @@ public final class PeertubeSearchQueryHandlerFactory extends SearchQueryHandlerF
 
     @Override
     public String getUrl(final String searchString,
-                         final List<String> contentFilters,
-                         final String sortFilter) throws ParsingException {
+                         final List<FilterItem> selectedContentFilter,
+                         final List<FilterItem> selectedSortFilters) throws ParsingException {
+
         final String baseUrl;
-        if (!contentFilters.isEmpty() && contentFilters.get(0).startsWith("sepia_")) {
+        final Optional<FilterItem> sepiaFilter =
+                PeertubeHelpers.getSepiaFilter(selectedContentFilter);
+        if (sepiaFilter.isPresent()) {
             baseUrl = SEPIA_BASE_URL;
         } else {
             baseUrl = ServiceList.PeerTube.getBaseUrl();
         }
-        return getUrl(searchString, contentFilters, sortFilter, baseUrl);
+
+        return getUrl(searchString, selectedContentFilter, selectedSortFilters, baseUrl);
     }
 
     @Override
     public String getUrl(final String searchString,
-                         final List<String> contentFilters,
-                         final String sortFilter,
+                         final List<FilterItem> selectedContentFilter,
+                         final List<FilterItem> selectedSortFilter,
                          final String baseUrl) throws ParsingException {
         try {
-            return baseUrl + SEARCH_ENDPOINT + "?search=" + Utils.encodeUrlUtf8(searchString);
+            searchFilters.setSelectedSortFilter(selectedSortFilter);
+            searchFilters.setSelectedContentFilter(selectedContentFilter);
+
+            final String filterQuery = searchFilters.evaluateSelectedFilters(null);
+
+            return baseUrl + SEARCH_ENDPOINT + "?search=" + Utils.encodeUrlUtf8(searchString)
+                    + filterQuery;
         } catch (final UnsupportedEncodingException e) {
             throw new ParsingException("Could not encode query", e);
         }
-    }
-
-    @Override
-    public String[] getAvailableContentFilter() {
-        return new String[]{
-                VIDEOS,
-                SEPIA_VIDEOS
-        };
     }
 }
