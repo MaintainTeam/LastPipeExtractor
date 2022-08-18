fe2456e927a642c2ce5bb9d8f23515a3b418fd9e
evermind <evermind@tuxfamily.org>
2022-08-19 00:48:22 +0200

    searchfilters: convert media_ccc to new framework
    Available content filters:
    - All
    - Conferences
    - Events

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/linkHandler/MediaCCCSearchQueryHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/media_ccc/linkHandler/MediaCCCSearchQueryHandlerFactory.java
@@ -2,33 +2,22 @@ package org.schabi.newpipe.extractor.services.media_ccc.linkHandler;
 
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.linkhandler.SearchQueryHandlerFactory;
+import org.schabi.newpipe.extractor.search.filter.FilterItem;
+import org.schabi.newpipe.extractor.services.media_ccc.search.filter.MediaCCCFilters;
 import org.schabi.newpipe.extractor.utils.Utils;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
-public class MediaCCCSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
-    public static final String ALL = "all";
-    public static final String CONFERENCES = "conferences";
-    public static final String EVENTS = "events";
+public final class MediaCCCSearchQueryHandlerFactory extends SearchQueryHandlerFactory {
 
-    @Override
-    public String[] getAvailableContentFilter() {
-        return new String[]{
-                ALL,
-                CONFERENCES,
-                EVENTS
-        };
+    public MediaCCCSearchQueryHandlerFactory() {
+        super(new MediaCCCFilters());
     }
 
     @Override
-    public String[] getAvailableSortFilter() {
-        return new String[0];
-    }
-
-    @Override
-    public String getUrl(final String query, final List<String> contentFilter,
-                         final String sortFilter) throws ParsingException {
+    public String getUrl(final String query, final List<FilterItem> contentFilter,
+                         final List<FilterItem> sortFilter) throws ParsingException {
         try {
             return "https://media.ccc.de/public/events/search?q=" + Utils.encodeUrlUtf8(query);
         } catch (final UnsupportedEncodingException e) {
